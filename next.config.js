fc6f4fb11e002a080ddeba034851ed1b31c0a126
Krish Kaushik <krishkaushk@cs.sfu.ca>
2026-03-27 01:14:50 -0700

    fixed file name on host

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/resume.pdf",
+        headers: [
+          {
+            key: "Content-Disposition",
+            value: 'attachment; filename="KrishZhaoKaushik_Resume.pdf"',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
